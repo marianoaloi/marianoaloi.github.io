628eb78c2256b76cfb9d9dccd637858250553d48
Mariano Aloi <mariano@aloi.com.br>
2024-04-12 14:26:28 -0300

    internacionalization

--- src/app/home.styled.tsx
+++ src/app/home.styled.tsx
@@ -1,8 +1,22 @@
 import styled from "@emotion/styled";
 import Image from "next/image";
+import { themeColor } from "./components/header.styled";
 
 
 
 export const MaloiImg = styled(Image)`
-margin-top: 10px;
+max-width: 200px;
+height: auto;
+margin: 10px;
+/* border: double 10px black; */
+background: white;
+margin-top: -2px;
+
+`
+
+export const GradientBoard = styled.div`
+    position: relative;
+    background: linear-gradient(to top, red, ${themeColor});
+    padding: 3px;
+    float: left;
 `
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
-import { MaloiImg } from "./home.styled";
+import { GradientBoard, MaloiImg } from "./home.styled";
 
 
 export default function Home() {
   
   return (
     <main >
-      <MaloiImg src={"./trabalhador_arquiteto_clean_600.png"}  width={600} height={503} alt="Solution Architect smilly" />
+      <GradientBoard>
+      <MaloiImg src={"./trabalhador_arquiteto_clean_600.png"}  width={416} height={503} alt="Solution Architect smilly" />
+      </GradientBoard>
     </main>
   );
 }
